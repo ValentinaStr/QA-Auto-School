0b5d19f7627120e26eb9edbc0694d84641f0d4fc
Валентина Стражникова <vvalentina_@hotmail.com>
2023-01-07 21:57:53 +0200

    Added methods for the triangle to the class

--- Program.cs
+++ Program.cs
@@ -7,41 +7,4 @@ Triangle First = new Triangle("First",
 		new Point(20,15)
 	}
 );
-Console.WriteLine(GetAreaOfTriangle(First));
-
-double GetSidesFirstLength(Triangle one)
-{
-	return one.TriangleApex[0].GetDistanse(one.TriangleApex[1]);
-}
-
-double GetSidesSecondLength(Triangle one)
-{
-	return one.TriangleApex[1].GetDistanse(one.TriangleApex[2]);
-}
-
-double GetSidesThirdLength(Triangle one) 
-{
-	return one.TriangleApex[2].GetDistanse(one.TriangleApex[0]);
-}
-
-double GetPerimetrTriangle(Triangle one)
-{
-	double firstSide = GetSidesFirstLength(one);
-	double secondSide = GetSidesSecondLength(one);
-	double thirdSide = GetSidesThirdLength(one);
-	 return firstSide + secondSide + thirdSide;
-}
-
-double GetAreaOfTriangle(Triangle one)
-{
-	double perimeter = GetPerimetrTriangle(one);
-	double firstSide = GetSidesFirstLength(one);
-	double secondSide = GetSidesSecondLength(one);
-	double thirdSide = GetSidesThirdLength(one);
-	return   Math.Cbrt(perimeter / 2 * 
-		 (perimeter / 2 - firstSide) * 
-		 (perimeter / 2 - secondSide) * 
-		 (perimeter / 2 - thirdSide)
-		 );
-
-}
+Console.WriteLine(First.GetAreaOfTriangle());
--- Triangle.cs
+++ Triangle.cs
@@ -1,6 +1,5 @@
 ﻿namespace DataTypesIntro
 {
-
 	internal class Triangle : Figure
 	{
 		public Point[] TriangleApex { get; set; }
@@ -10,6 +9,40 @@
 			TriangleApex = triangleApex;
 		}
 
-		//First.TriangleApex[0].GetDistanse(First.TriangleApex[1])
+		public double GetSidesFirstLength()
+		{
+			return TriangleApex[0].GetDistanse(TriangleApex[1]);
+		}
+		public double GetSidesSecondLength()
+		{
+			return TriangleApex[1].GetDistanse(TriangleApex[2]);
+		}
+		public double GetSidesThirdLength()
+		{
+			return TriangleApex[2].GetDistanse(TriangleApex[0]);
+		}
+
+		public double GetPerimetrTriangle()
+		{
+			double firstSide = GetSidesFirstLength();
+			double secondSide = GetSidesSecondLength();
+			double thirdSide = GetSidesThirdLength();
+			return firstSide + secondSide + thirdSide;
+		}
+
+		public double GetAreaOfTriangle()
+		{
+			double perimeter = GetPerimetrTriangle();
+			double firstSide = GetSidesFirstLength();
+			double secondSide = GetSidesSecondLength();
+			double thirdSide = GetSidesThirdLength();
+			return Math.Cbrt(perimeter / 2 *
+				 (perimeter / 2 - firstSide) *
+				 (perimeter / 2 - secondSide) *
+				 (perimeter / 2 - thirdSide)
+				 );
+
+		}
 	}
+
 }
