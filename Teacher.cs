ef46aaa287c9ff3b3092690e1aa191e5aa28c33e
Валентина Стражникова <vvalentina_@hotmail.com>
2023-01-08 23:27:06 +0200

    Add class Teacher

---
+++ Teacher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypesIntro
+{
+	internal class Teacher : UniversityEmployee
+	{
+		public Course Course { get; set; }
+
+		public Teacher(employeePerson, taxID, Course course) : base(employeePerson, taxID)
+		{
+			Course = course;
+		}
+	}
+}
---
+++ UniversityEmployee.cs
@@ -0,0 +1,22 @@
+﻿using DataTypesIntro.Candidates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypesIntro
+{
+	internal class UniversityEmployee
+	{
+		private int _taxID;
+		public Person EmployeePerson { get; set; }
+		public int TaxID { get; set; }
+
+		public UniversityEmployee(Person employeePerson, int taxID)
+		{
+			EmployeePerson = employeePerson;
+			TaxID = taxID;
+		}
+	}
+}
