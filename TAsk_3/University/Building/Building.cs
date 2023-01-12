bcf024c862c7d1ae5f0fdf7307063f806746580d
Валентина Стражникова <vvalentina_@hotmail.com>
2023-01-12 13:09:39 +0200

    Task 3 opened

---
+++ TAsk_3/University/Building/Building.cs
@@ -0,0 +1,18 @@
+﻿namespace Task_3
+{
+	internal class Building
+	{
+		public int Number { get; set; }
+		public string Name { get; set; }
+		public Room[] Rooms { get; set; }
+		public AddressBulding AddressBulding { get; set; }
+
+		public Building(int number, string name, Room[] rooms, AddressBulding addressBulding)
+		{
+			Number = number;
+			Name = name;
+			Rooms = rooms;
+			AddressBulding = addressBulding;
+		}
+	}
+}
---
+++ TAsk_3/University/University.cs
@@ -0,0 +1,18 @@
+﻿using DataTypesIntro;
+using DataTypesIntro.Candidates;
+
+namespace Task_3
+{
+	internal class University
+	{
+		internal Person Rector { get; set; }
+		internal UniversityEmployee[] AllUniversityEmployees { get; set; }
+		internal Building[] Allbuildings { get; set; }
+		public University(Person rector, UniversityEmployee[] allUniversityEmployees, Building[] allbuildings)
+		{
+			Rector = rector;
+			AllUniversityEmployees = allUniversityEmployees;
+			Allbuildings = allbuildings;
+		}
+	}
+}
