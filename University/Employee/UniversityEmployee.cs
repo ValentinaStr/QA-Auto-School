c1398536a506c0b9c1aaf833e6469a9d6197ba10
Валентина Стражникова <vvalentina_@hotmail.com>
2023-01-25 17:49:59 +0200

    Change TaxId

--- University/Employee/UniversityEmployee.cs
+++ University/Employee/UniversityEmployee.cs
@@ -5,12 +5,12 @@ namespace University.Employee
 	public abstract class UniversityEmployee
 	{
 		public Person EmployeePerson { get; set; }
-		public int TaxID { get; set; }
+		public int TaxId { get; set; }
 
 		public UniversityEmployee(Person employeePerson, int taxID)
 		{
 			EmployeePerson = employeePerson;
-			TaxID = taxID;
+			TaxId = taxID;
 		}
 		public abstract string GetOfficialDuties();
 	}
