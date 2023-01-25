36bf480d7f0bb8467976f3ffb82f78f61ff038ce
Валентина Стражникова <vvalentina_@hotmail.com>
2023-01-25 17:47:49 +0200

    Task_3,4,5

---
+++ Building_Linq/RealProperty/Room.cs
@@ -0,0 +1,19 @@
+﻿namespace Building_LINQ.RealProperty
+{
+    public class Room
+    {
+        public int NumberRoom { get; set; }
+        public string Name { get; set; }
+
+        public Room(int numder, string name)
+        {
+            NumberRoom = numder;
+            Name = name;
+        }
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+            return obj is Room room && room != null && room.NumberRoom == NumberRoom;
+        }
+    }
+}
