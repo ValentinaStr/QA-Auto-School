0d318de3ee2efabda885c0f2111f7d5250254a53
Валентина Стражникова <vvalentina_@hotmail.com>
2023-02-06 14:04:29 +0200

    Fixed Unit test Room

--- UniversityUnitTests/RoomUnitTests.cs
+++ UniversityUnitTests/RoomUnitTests.cs
@@ -7,21 +7,24 @@ namespace UniversityUnitTests
 	public class RoomUnitTests
 	{
 		[TestMethod]
-		public void СompareRoomWithTheSameNumberPositive()
+		public void RoomWithTheSameNumberPositive()
 		{
 			var resultRoom1 = new Room(5, "Room name");
 			var resultRoom2 = new Room(3, "Room name");
 
 			Assert.IsFalse(resultRoom1.Equals(resultRoom2));
+			Assert.AreNotEqual(resultRoom1.GetHashCode(), resultRoom2.GetHashCode());
+
 		}
 
 		[TestMethod]
-		public void СompareRoomWithTheSameNumberNegative()
+		public void RoomWithTheSameNumberNegative()
 		{
 			var resultRoom1 = new Room(3, "Room name");
 			var resultRoom2 = new Room(3, "Room name");
 
 			Assert.IsTrue(resultRoom1.Equals(resultRoom2));
+			Assert.AreEqual(resultRoom1.GetHashCode(), resultRoom2.GetHashCode());
 		}
 	}
 }
